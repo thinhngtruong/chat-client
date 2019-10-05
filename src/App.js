2221ba4a14f5c7e80247a290c1283503c780f43b
Thinh Nguyen <55010366+zekeyeager@users.noreply.github.com>
2019-10-06 02:07:14 +0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -22,7 +22,7 @@ export default class App extends React.Component {
     }
     //Connetct với server nodejs, thông qua socket.io
     UNSAFE_componentWillMount() {
-        this.socket = io('localhost:4000');
+        this.socket = io('https://chat-server-2019.herokuapp.com/');
         this.socket.on('newMessage', (response) => { this.newMessage(response) }); //lắng nghe khi có tin nhắn mới
         this.socket.on('loginFail', (response) => { alert('Tên đã có người sử dụng') }); //login fail
         this.socket.on('loginSuccess', (response) => { this.setState({ user: { id: this.socket.id, name: response } }) }); //đăng nhập thành công 
